c5adb52c7c3d2412a2f197efad1700533861c486
Cristian Monzon Guzman <Cristian-02-@live.com>
2022-04-16 23:01:10 -0500

    add the calculator

--- app/src/main/java/com/example/tarea_cristianmonzonguzman/Maps/MapsFragment.java
+++ app/src/main/java/com/example/tarea_cristianmonzonguzman/Maps/MapsFragment.java
@@ -1,4 +1,4 @@
-package com.example.tarea_cristianmonzonguzman.Calculator;
+package com.example.tarea_cristianmonzonguzman.Maps;
 
 import android.os.Bundle;
 
@@ -7,18 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.example.tarea_cristianmonzonguzman.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CalculatorFragment#newInstance} factory method to
+ * Use the {@link MapsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CalculatorFragment extends Fragment {
-
-    Button btnLog;
+public class MapsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,11 +26,9 @@ public class CalculatorFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public CalculatorFragment() {
+    public MapsFragment() {
         // Required empty public constructor
-
     }
-
 
     /**
      * Use this factory method to create a new instance of
@@ -41,17 +36,16 @@ public class CalculatorFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment CalculatorFragment.
+     * @return A new instance of fragment MapsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CalculatorFragment newInstance(String param1, String param2) {
-        CalculatorFragment fragment = new CalculatorFragment();
+    public static MapsFragment newInstance(String param1, String param2) {
+        MapsFragment fragment = new MapsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-
     }
 
     @Override
@@ -61,28 +55,12 @@ public class CalculatorFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_calculator, container, false);
-
-//        btnLog = view.findViewById(R.id.txtResult);
-//
-//        btnLog.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                System.out.println("Holaaaa");
-//            }
-//        });
-
         // Inflate the layout for this fragment
-        return view;
-
-
+        return inflater.inflate(R.layout.fragment_maps, container, false);
     }
 }
